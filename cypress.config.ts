81b0b076316b909cba37a08894e408a1b841c9de
Frank van Wijk <info@frankvanwijk.nl>
2022-11-18 13:57:05 +0100

    Log errors in a table

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,18 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        log(message) {
+          console.log(message);
+
+          return null;
+        },
+        table(message) {
+          console.table(message);
+
+          return null;
+        },
+      });
     },
   },
 });
