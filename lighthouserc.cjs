f1443402185256726e7e000b3a369972f48f2db4
Bart den Hollander <None>
2022-11-18 14:31:24 +0100

    add to server

---
+++ lighthouserc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+      upload: {
+        target: 'lhci',
+        serverBaseUrl: 'https://boiling-hamlet-84440.herokuapp.com',
+        token: 'ac77c13c-0b03-4c0d-b4bd-5cc02648d162', // could also use LHCI_TOKEN variable instead
+      },
+    },
+  };
+
+  
