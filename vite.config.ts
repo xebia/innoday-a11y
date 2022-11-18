e542989fb3a0cb085205438dcf21b2bf1eea4549
Frank van Wijk <info@frankvanwijk.nl>
2022-11-18 11:16:16 +0100

    Start server in CI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    strictPort: true,
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["vitest-setup.ts"],
