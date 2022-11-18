e1c8b41b466f0d79fc9df890876bb7989383f88b
Frank van Wijk <info@frankvanwijk.nl>
2022-11-18 13:45:08 +0100

    Check for a11y

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,15 @@
-describe("empty spec", () => {
-  it("passes", () => {
+describe("Successful page", () => {
+  it("passes a11y check", () => {
+    cy.visit("/success");
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+});
+
+describe("Failing page", () => {
+  it("fails a11y check", () => {
     cy.visit("/");
+    cy.injectAxe();
+    cy.checkA11y();
   });
 });
